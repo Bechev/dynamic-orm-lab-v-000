bc921dc04047a73b923f5eedd10026080a285de4
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-11 14:23:27 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -31,8 +31,10 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    # binding.pry
     self.class.column_names.delete_if{|col| col == "id"}.join(", ")
+  end
+
+  def values_for_insert
   end
 
 end
